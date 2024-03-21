be1df6e8e5c9943ded39c2affc80132511c3dced
Yevhen Martyniuk <116523129+evgenijmartynuk07@users.noreply.github.com>
2024-03-21 11:05:26 +0200

    Update page.py

--- framework/page.py
+++ framework/page.py
@@ -13,7 +13,7 @@ class Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def _find_element(self, locator: tuple):
+    def find_element(self, locator: tuple):
         strategy, value = locator
         strategy = strategy.lower()
         supported_strategies = {'id': AppiumBy.ID, 'xpath': AppiumBy.XPATH}
@@ -35,31 +35,31 @@ class Page:
                 f"Unsupported value: {value} or {strategy}. Please check!"
             )
 
-    def _clear_element(self, element):
+    def clear_element(self, element):
         try:
             element.clear()
         except StaleElementReferenceException:
-            element = self._find_element(element)
+            element = self.find_element(element)
             element.clear()
 
-    def _send_keys_to_element(self, element, keys):
+    def send_keys_to_element(self, element, keys):
         try:
             element.send_keys(keys)
         except StaleElementReferenceException:
-            element = self._find_element(element)
+            element = self.find_element(element)
             element.send_keys(keys)
 
     def get_back(self):
         self.driver.back()
 
     @staticmethod
-    def _click_element(element):
+    def click_element(element):
         try:
             element.click()
         except WebDriverException as e:
             raise f'Error clicking element: {e}'
 
-    def _find_and_click_element(self, element_locator):
-        element = self._find_element(element_locator)
+    def find_and_click_element(self, element_locator):
+        element = self.find_element(element_locator)
         if element:
-            self._click_element(element)
+            self.click_element(element)
