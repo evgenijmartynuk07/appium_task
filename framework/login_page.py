414d8b4d9083f5818f9fb3950d2fc8df19d1b25d
Yevhen Martyniuk <116523129+evgenijmartynuk07@users.noreply.github.com>
2024-03-21 11:05:58 +0200

    Update login_page.py

--- framework/login_page.py
+++ framework/login_page.py
@@ -6,28 +6,28 @@ class LoginPage(Page):
 
     def login(self, username, password):
 
-        self._find_and_click_element(Locator.LOGIN_BUTTON)
+        self.find_and_click_element(Locator.LOGIN_BUTTON)
 
-        username_input = self._find_element(Locator.USERNAME_INPUT)
-        self._clear_element(username_input)
-        self._send_keys_to_element(username_input, username)
+        username_input = self.find_element(Locator.USERNAME_INPUT)
+        self.clear_element(username_input)
+        self.send_keys_to_element(username_input, username)
 
-        password_input = self._find_element(Locator.PASSWORD_INPUT)
-        self._clear_element(password_input)
-        self._send_keys_to_element(password_input, password)
+        password_input = self.find_element(Locator.PASSWORD_INPUT)
+        self.clear_element(password_input)
+        self.send_keys_to_element(password_input, password)
 
-        self._find_and_click_element(Locator.SUBMIT_BUTTON)
+        self.find_and_click_element(Locator.SUBMIT_BUTTON)
 
     def logout(self):
-        self._find_and_click_element(Locator.SIDEBAR_BUTTON)
+        self.find_and_click_element(Locator.SIDEBAR_BUTTON)
 
-        self._find_and_click_element(Locator.SETTINGS_BUTTON)
+        self.find_and_click_element(Locator.SETTINGS_BUTTON)
 
-        self._find_and_click_element(Locator.LOGOUT_BUTTON)
+        self.find_and_click_element(Locator.LOGOUT_BUTTON)
 
     def is_logged_in(self):
         try:
-            element = self._find_element(Locator.SIDEBAR_BUTTON)
+            element = self.find_element(Locator.SIDEBAR_BUTTON)
             if element:
                 return True
             return False
